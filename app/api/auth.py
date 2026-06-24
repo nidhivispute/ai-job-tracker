9278bd59c68c49853a35de6c77af1b768f9dc6bf
Nidhi Vispute <nidhi.vispute2@gmail.com>
2026-06-23 23:35:20 -0400

    Add user endpoint

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
+from app.api.deps import get_current_user
 from app.db.database import get_db
+from app.models.user import User
 from app.schemas.token import Token
 from app.schemas.user import UserCreate, UserLogin, UserRead
 from app.services.auth_service import create_user, login_user
@@ -18,3 +20,8 @@ def signup(user_data: UserCreate, db: Session = Depends(get_db)):
 @router.post("/login", response_model=Token)
 def login(user_data: UserLogin, db: Session = Depends(get_db)):
     return login_user(db, user_data)
+
+
+@router.get("/me", response_model=UserRead)
+def get_me(current_user: User = Depends(get_current_user)):
+    return current_user
